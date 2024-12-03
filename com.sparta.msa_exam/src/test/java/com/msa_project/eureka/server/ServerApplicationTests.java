0a1294df4a10c4714ed0ce635f4774217ba4cada
pk_hyun <pkh954533@gmail.com>
2024-12-03 12:57:07 +0900

    init commit

---
+++ com.sparta.msa_exam/src/test/java/com/msa_project/eureka/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msa_project.eureka.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
